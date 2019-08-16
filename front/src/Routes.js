5675a7541f56b0fb384769bd3132916a97ed798a
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-08-16 22:46:20 +0600

    Подключила компоненты юзера в роут

---
+++ front/src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Route, Switch} from "react-router-dom";
+
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+
+const Routes = ({user}) => {
+  return (
+    <Switch>
+      <Route path="/register" exact component={Register}/>
+      <Route path="/login" exact component={Login}/>
+    </Switch>
+  );
+};
+
+export default Routes;
