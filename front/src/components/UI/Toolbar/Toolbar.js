f39774396dd77fc2ec5ceea1e3bf962f721f4b06
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-08-16 22:30:23 +0600

    Подключила меню для анонимного и зарегистрированного пользователей

---
+++ front/src/components/UI/Toolbar/Toolbar.js
@@ -0,0 +1,21 @@
+import React, {Fragment} from 'react';
+import {Nav, Navbar, NavbarBrand} from "reactstrap";
+import {NavLink as RouterNavLink} from "react-router-dom";
+
+import AnonymousMenu from "../Menus/AnonymousMenu";
+import UserMenu from "../Menus/UserMenu";
+
+const Toolbar = ({user,logout}) => {
+  return (
+    <Fragment>
+      <Navbar color="info" light expand="md">
+        <NavbarBrand tag={RouterNavLink} to={user && user.role === 'admin' ? "/admin" : "/"}>Recipes</NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          {user ? <UserMenu user={user} logout={logout}/>: <AnonymousMenu/>}
+        </Nav>
+      </Navbar>
+    </Fragment>
+  );
+};
+
+export default Toolbar;
