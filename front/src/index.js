94b3b65ff7b433bcaf0a2bbe0debc4e6cd7be98f
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-08-16 22:48:07 +0600

    Подключила App.js и редюсер к комбайну

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createBrowserHistory} from 'history';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import {connectRouter, routerMiddleware, ConnectedRouter} from 'connected-react-router';
 import * as serviceWorker from './serviceWorker';
+import axios from './axios-api';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-notifications/lib/notifications.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import App from './App';
+
+import {loadFromLocalStorage, saveToLocalStorage} from "./store/localStorage";
+import userReducer from "./store/reducers/userReducer";
+
+const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+  router: connectRouter(history),
+  users: userReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = [
+  thunkMiddleware,
+  routerMiddleware(history)
+];
+
+const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(rootReducer, persistedState, enhancers);
+
+store.subscribe(() => {
+  saveToLocalStorage({
+    users: {
+      user: store.getState().users.user
+    }
+  })
+});
+
+axios.interceptors.request.use(config => {
+  try {
+    config.headers['Authorization'] = store.getState().users.user.token;
+  } catch(e) {
+    // do nothing, user is not logged in
+  }
+
+  return config
+});
+
+const app = (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App/>
+    </ConnectedRouter>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 serviceWorker.unregister();
+
