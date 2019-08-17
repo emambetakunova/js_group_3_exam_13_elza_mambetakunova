092bc8ddbff74b57ddafaae2a1b8aae9611bc408
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-08-17 11:00:17 +0600

    Подключила главную страницу и страницу добавления заведений

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -3,12 +3,16 @@ import {Route, Switch} from "react-router-dom";
 
 import Register from "./containers/Register/Register";
 import Login from "./containers/Login/Login";
+import AddPlace from "./containers/AddPlace/AddPlace";
+import PlaceBuilder from "./containers/PlaceBuilder/PlaceBuilder";
 
 const Routes = ({user}) => {
   return (
     <Switch>
+      <Route path="/" exact component={PlaceBuilder}/>
       <Route path="/register" exact component={Register}/>
       <Route path="/login" exact component={Login}/>
+      <Route path="/add" exact component={AddPlace}/>
     </Switch>
   );
 };
