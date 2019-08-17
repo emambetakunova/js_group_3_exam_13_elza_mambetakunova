53c6b1eb3adcbae81b80adb8f7aa8195dfd73f37
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-08-17 18:28:16 +0600

    Добавила action для удаления заведений, отывов и фото

--- front/src/store/actions/photoAction.js
+++ front/src/store/actions/photoAction.js
@@ -5,8 +5,14 @@ import axios from "../../axios-api";
 export const FETCH_PHOTO_SUCCESS = 'FETCH_PHOTO_SUCCESS';
 export const FETCH_PHOTO_FAILURE = "FETCH_PHOTO_FAILURE";
 
+export const DELETE_IMAGE_SUCCESS = 'DELETE_IMAGE_SUCCESS';
+export const DELETE_IMAGE_FAILURE = "DELETE_IMAGE_FAILURE";
+
 const fetchPhotosSuccess = photos => ({type: FETCH_PHOTO_SUCCESS, photos});
 const fetchPhotosFailure = error => ({type: FETCH_PHOTO_FAILURE, error});
+
+const deleteImageSuccess = () => ({type: DELETE_IMAGE_SUCCESS});
+const deleteImageFailure = error => ({type: DELETE_IMAGE_FAILURE, error});
 
 export const fetchPhotos = (id) => {
   return dispatch => {
@@ -25,5 +31,24 @@ export const addPhoto = (data, id) => {
         dispatch(fetchPhotos(id));
         NotificationManager.success('Photos added')
       })
+  }
+};
+
+export const deleteImage = (id) => {
+  return dispatch => {
+    return axios.delete('/places/' + id).then(
+      () => {
+        dispatch(deleteImageSuccess());
+        NotificationManager.success('Deleted successfully');
+      },
+      error => {
+        if (error.response && error.response.data) {
+          dispatch(deleteImageFailure(error.response.data));
+        } else {
+          dispatch(deleteImageFailure({global: 'No connection'}))
+        }
+
+      }
+    )
   }
 };
