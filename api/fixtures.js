af537246962dcb1a3f4c294adc9b4f8b34d79a3d
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-08-17 10:57:48 +0600

    Добавила фикстурные данные

--- api/fixtures.js
+++ api/fixtures.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 const User = require('./models/User');
+const Place = require('./models/Place');
+const Review = require('./models/Review');
 
 const run = async () => {
   await mongoose.connect(config.dbUrl, config.mongoOptions);
@@ -26,6 +28,45 @@ const run = async () => {
       password: '123',
       token: 'token1',
       role: 'admin'
+    }
+  );
+
+  const place = await Place.create(
+    {
+      user: users[0],
+      title: 'Bellagio',
+      description: 'Redefining the concept of buffet dining with impressive live-action cooking stations and an impeccable selection of cuisines unmatched in Las Vegas.',
+      image: 'place.jpeg'
+    },
+    {
+      user: users[0],
+      title: 'Tyubeteyka',
+      description: 'Tyubeteyka” restaurant is the place of inexpensive and tasty dinner.',
+      image: 'place.jpeg'
+    },
+    {
+      user: users[0],
+      title: 'IT Ibiza',
+      description: 'IT Ibiza is not only a restaurant but our daily effort to celebrate beauty, taste and simplicity. ',
+      image: 'place.jpeg'
+    }
+  );
+
+  await Review.create(
+    {
+      user: users[0],
+      place: place[0],
+      review: 'So cool!'
+    },
+    {
+      user: users[0],
+      place: place[1],
+      review: 'Lol!'
+    },
+    {
+      user: users[0],
+      place: place[1],
+      review: 'So cool!'
     }
   );
 
