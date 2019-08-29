83a1da75a8005fb8db269fa50809842acc7a918e
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-08-29 22:47:24 +0600

    Удалила пост запрос на добавления фотографий, так как перенесла в роуты заведений

--- api/app/photos.js
+++ api/app/photos.js
@@ -5,25 +5,20 @@ const auth = require('../middleWare/auth');
 const permit = require('../middleWare/permit');
 const Photo = require('../models/Photo');
 
+router.get('/', async (req, res) => {
+  Photo.find()
+    .then(result => {
+      if (result) return res.send(result);
+      res.sendStatus(404)
+    })
+    .catch(error => res.status(500).send(error));
+
+});
+
 router.get('/:id', async (req, res) => {
   try {
     const review = await Photo.find({place: req.params.id}).populate('user');
     return res.send(review)
-  } catch (error) {
-    return res.status(400).send(error)
-  }
-});
-
-
-router.post('/:id', [auth], async (req, res) => {
-  try {
-    const photo = new Photo({
-      user: req.user._id,
-      place: req.body.place,
-      images: req.body.images
-    });
-    photo.save();
-    return res.send(photo);
   } catch (error) {
     return res.status(400).send(error)
   }
@@ -34,5 +29,6 @@ router.delete('/:id', [auth, permit('admin')], async (req, res) => {
 
   res.send('success');
 });
+
 
 module.exports = router;
